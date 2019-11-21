501ff14d4ff97d86dc35e8087b425469724c3d12
Pvl <stepanov2003@mail.ru>
2019-11-21 16:13:22 +0300

    Initial commit

---
+++ src/com/company/AnimalTransport.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public abstract class AnimalTransport {
+    abstract void say();
+    abstract void canSwim();
+    abstract void canRun();
+    abstract void canFly();
+
+
+}
---
+++ src/com/company/Fish.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Fish implements AnimalTransport {
+    @Override
+    void say() {
+        double x;
+        double y;
+        return x+y;
+    }
+
+    @Override
+    void canSwim() {
+
+    }
+
+    @Override
+    void canRun() {
+
+    }
+
+    @Override
+    void canFly() {
+
+    }
+}
